ff05639e0429a12a4fd4e293738d2a14d98abeee
Kaiki Mello <kaiki.mello@gmail.com>
2020-10-31 20:26:30 -0300

    Aula sobre operação aritimética

---
+++ OperacoesAritmeticas.dart
@@ -0,0 +1,47 @@
+void main() {
+
+  //Declarando dois números 
+  double num1 = 10.0;
+  double num2 = 20.0;
+
+  //Calculando soma e atribuindo na variável result 
+  double result = num1 + num2;
+
+  //Imprimindo o resultado
+  print(result);
+
+  //Calculando multiplicação e atribuindo na variável result 2 
+  double result2 = num1 * num2;
+
+  print(result2);
+
+  //Calculando multiplicação com expressão em parênteses 
+  result2 = (num1 * num2) - 30.0;
+
+  print(result2);
+
+  //Operador resumido
+
+  num1 += 15;
+
+  print(num1);
+
+  //Para efetuar esse mesmo cálculo seria num1 = num1 + 15
+
+  //Incrementa mais 1
+  num1++;
+
+  print(num1);
+
+  //Decrementa 1
+  num1--;
+
+  print(num1);
+
+  //Multiplica 
+  num1 *= 15;
+
+  print(num1);
+
+
+}
